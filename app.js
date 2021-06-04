a264df3859b9d4d2e6cbcf8dd78ae0d534da44c2
Rentsen <rentsena1@gmail.com>
2021-06-04 23:59:40 +0800

    Shineer ehluuldeg bolgoson

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-//Тоглогчийн ээлжийг хадгалах хувьсагч ,нэгдүгээр тоглогчийг A ,хоёрдугаар тоглогчийг Bгэж тэмдэглэе
-var activePlayer = 0;
-//Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
-var score = [0,0];
-//Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хуьсагч
-var roundScore=0;
+
 //Шооны алт талаараа буусныг хадгалах хувьсагч хэрэгтэй , 1-6 гэсэн утгыг энэ хувьсагчид снамсаргүйгээр үүсгэж өгнө.
 
 // <div class="player-score" id="score-0">43</div>
@@ -13,11 +8,7 @@ var roundScore=0;
 
 //document.querySelector('.dice')
 //console.log("Шоо "+dice);
-//Эхлэхэд бэлтгэх:
-// <div class="player-score" id="score-0">43</div>
-// Query gee iluu hurdan ajilladag
-document.getElementById("score-0").textContent="0";
-document.getElementById("score-1").textContent="0";
+var activePlayer,score,roundScore;
 
 //document.querySelector('#score-0').textContent=0;
 //document.querySelector('#score-1').textContent=0;
@@ -26,13 +17,32 @@ document.getElementById("score-1").textContent="0";
 //document.querySelector('score-1').textContent=dice;
 //Шоо зураг хайх <img src="dice-5.png" alt="Dice" class="dice">
 var diceDom = document.querySelector(".dice");
-diceDom.style.display="none";
-//авсан оноо 
+function initGame()
+{
+
+    //Тоглогчийн ээлжийг хадгалах хувьсагч ,нэгдүгээр тоглогчийг A ,хоёрдугаар тоглогчийг Bгэж тэмдэглэе
+ activePlayer = 0;
+//Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
+ score = [0,0];
+//Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хуьсагч
+ roundScore=0;
+//Эхлэхэд бэлтгэх:
+// <div class="player-score" id="score-0">43</div>
+// Query gee iluu hurdan ajilladag
+document.getElementById("score-0").textContent="0";
+document.getElementById("score-1").textContent="0";
 //<div class="player-current-score" id="current-0">11</div>
 //document.querySelector('#current-0').textContent=0;
 document.getElementById("current-0").textContent="0";
 document.getElementById("current-1").textContent="0";
 //document.querySelector('#current-1').textContent=0;
+diceDom.style.display="none";
+document.getElementById('name-0').textContent='Player 1';
+document.getElementById('name-0').textContent='Player 2';
+}
+
+//авсан оноо 
+
 //Товч хийх шоо шидэх  эвент листенер
 
 document.querySelector(".btn-roll").addEventListener("click",function ()
@@ -52,19 +62,46 @@ document.querySelector(".btn-roll").addEventListener("click",function ()
     }
     else
     {
-        roundScore=0;
-        //нэг ээлжинд олсон оноог 0 болгож өөрчиднө
-        document.getElementById("current-"+activePlayer).textContent=0;
-        //Тоглогчийн ээлжийг солих
-        activePlayer===0?(activePlayer=1):(activePlayer =0);
-        
-       // Улаан цэгийг шилжүүлэх
-       document.querySelector(".player-0-panel").classList.toggle("active");
-       document.querySelector(".player-1-panel").classList.toggle("active");
-       // Шоог түр алга болгох
-       diceDom.style.display="none";
+        switchNextplayer();
     }
 
     
 });
-x   
+//Hold товч 
+document.querySelector(".btn-hold").addEventListener("click",function()
+{
+    //Уг тоглогчийн ээлжийн оноог глобаль дээр харуулах
+    score[activePlayer]+=roundScore;
+    // Оноог дэлгэцэнд харуулах
+    document.getElementById('score-'+activePlayer).textContent=score[activePlayer];
+
+    // Toglogch hojson esehiig shalgah
+    if(score[activePlayer]>=100)
+    {
+        // Yalagchiin nernii orond yalsan bichig
+        document.getElementById('name-'+activePlayer).textContent='Winner';
+    }
+  else
+  {
+      switchNextplayer();
+  }
+    
+});
+// Тоглогчийн ээлжийг солих функц
+function switchNextplayer()
+{
+    roundScore=0;
+    //нэг ээлжинд олсон оноог 0 болгож өөрчиднө
+    document.getElementById("current-"+activePlayer).textContent=0;
+    //Тоглогчийн ээлжийг солих
+    activePlayer===0?(activePlayer=1):(activePlayer =0);
+    
+   // Улаан цэгийг шилжүүлэх
+   document.querySelector(".player-0-panel").classList.toggle("active");
+   document.querySelector(".player-1-panel").classList.toggle("active");
+   // Шоог түр алга болгох
+   diceDom.style.display="none";
+}
+
+// Shineer ehluuleh
+document.querySelector('.btn-new').addEventListener('click',initGame);
